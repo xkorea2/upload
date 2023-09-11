a1310ed596f949e400bc383f45356e2d629f71f8
xkorea2 <xkorea2@gmail.com>
2023-09-11 23:53:44 +0900

    feat : first commit

---
+++ src/main/java/hello/upload/domain/UploadFile.java
@@ -0,0 +1,10 @@
+package hello.upload.domain;
+
+public class UploadFile {
+
+    private String uploadFileName;
+    private String storeFileName;
+
+    public UploadFile(String originalFilename, String storeFileName) {
+    }
+}
